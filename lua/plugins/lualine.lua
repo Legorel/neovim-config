ef7a15b6f70ced87a713f02d6840b834b64c2959
Legorel <legorel412@gmail.com>
2024-03-02 22:17:54 +0100

    Add buffers to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,4 +2,12 @@ require('lualine').setup {
 	options = {
 		theme = 'tokyonight',
 	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diff', 'diagnostic' },
+		lualine_c = { 'filename', 'buffers' },
+		lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' },
+	},
 }
