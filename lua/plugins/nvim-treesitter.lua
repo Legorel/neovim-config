d3c0423e9c9f58cedd38d712e76bc2f60d7ade65
Legorel <legorel412@gmail.com>
2024-03-19 14:57:48 +0100

    Fix modules not being enabled

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,18 @@ require('nvim-treesitter.configs').setup {
 	ensure_installed = { 'c', 'cpp', 'bash', 'python', 'lua', 'java', 'ocaml' },
 	auto_install = false,
 	highlight = {
-		enabled = true,
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
 	},
 }
